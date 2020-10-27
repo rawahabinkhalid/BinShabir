27445b047f7de34bdee6d88956498eef2fc65411
AbdulRafayMirza <hafizabdulrafay7@gmail.com>
2020-10-27 13:06:13 +0500

    ARB_27102020_1

--- BiltyBillsSubmit.php
+++ BiltyBillsSubmit.php
@@ -1,16 +1,12 @@
 <?php
 include_once 'conn.php';
 
+
+
 for ($i = 0; $i < count($_POST['date']); $i++) {
     $sql1 =
         "INSERT INTO bilty_bill (`Date`,`Reference_Name`,`Bill_No`,`Reference_Contact`,`Amount_Received`)
-                            VALUES ( '" .
-        $_POST['date'][$i] .
-        "','" .
-        $_POST['referencename'][$i] .
-        "','" .
-        $_POST['billno'][$i] .
-        "','" .
+                            VALUES ( '".$_POST['date'][$i]."','".$_POST['referencename'][$i] ."','".$_POST['billno'][$i]."','" .
         $_POST['referencecontract'][$i] .
         "','" .
         $_POST['totalamountreceived'][$i] .
